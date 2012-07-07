d0fdec5d5278d70d42c505d57bea0b7e053c0aeb
Stein Eldar Johnsen <morimekta@gmail.com>
2012-07-07 18:43:52 +0200

    Back to local and reload.

--- src/uk/ac/ucl/casa/cosmet/MainActivity.java
+++ src/uk/ac/ucl/casa/cosmet/MainActivity.java
@@ -3,11 +3,17 @@ package uk.ac.ucl.casa.cosmet;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
 	private WebView webView;
+	private Button statsButton;
+	// protected String statsUrl = "http://192.168.145.243:8000/index.html";
+
+	protected String statsUrl = "http://128.40.111.232/cosmet/cosmet-gtv-html/index.html";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,15 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		this.webView = (WebView) findViewById(R.id.graph_view);
+
+		this.statsButton = (Button) findViewById(R.id.my_status_button);
+
+		this.statsButton.setClickable(true);
+		this.statsButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+				webView.loadUrl(statsUrl);
+			}
+		});
 	}
 
 	@Override
@@ -22,12 +37,13 @@ public class MainActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onStart();
 
-		this.webView.loadUrl("http://128.40.111.232/cosmet/cosmet-gtv-html/index.html");
+		this.webView.loadUrl(statsUrl);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
+
 		return true;
 	}
 }
